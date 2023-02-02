e6575a12cc656fa6075a26d1a618d5ac5878cd31
deon-gracias <deongracias1@gmail.com>
2023-02-02 08:42:34 +0530

    Added register user with email

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -7,6 +7,16 @@ export async function signInWithEmail(email: string, password: string) {
   return await usersCollection.authWithPassword(email, password);
 }
 
+export async function registerWithEmail(
+  username: string,
+  email: string,
+  password: string
+) {
+  return await pb
+    .collection("users")
+    .create({ username, email, password, passwordConfirm: password });
+}
+
 export function logout() {
   pb.authStore.clear();
 }
