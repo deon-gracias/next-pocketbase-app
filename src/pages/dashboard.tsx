5fab5b461b690b58f49ac95ac683eaa079bc03bc
deon-gracias <deongracias1@gmail.com>
2023-02-02 07:24:02 +0530

    Added `DashboardLayout` and `DashboardHeader`

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,18 @@
+import DashboardHeader from "@/components/dashboard/DashboardHeader";
+import DashboardLayout from "@/components/dashboard/DashboardLayout";
+import { useUserStore } from "@/store/userStore";
+import Head from "next/head";
+
+export default function Dashboard() {
+  const { user } = useUserStore();
+
+  return (
+    <DashboardLayout>
+      <Head>
+        <title>Dashboard</title>
+      </Head>
+
+      <DashboardHeader title={"Dashboard"} />
+    </DashboardLayout>
+  );
+}
